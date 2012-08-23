8f00c25d41c131474b29b71e07f868d5e6a890ce
christoc_cp <SND\christoc_cp>
2012-08-23 21:06:43 +0000

    VSIX Appears to be working, checking in so we can do some final package work and then start testing.

--- VSIX/Templates/DotNetNuke 6 VB Compiled Module/Settings.ascx.vb
+++ VSIX/Templates/DotNetNuke 6 VB Compiled Module/Settings.ascx.vb
@@ -7,7 +7,7 @@
 ' CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 ' DEALINGS IN THE SOFTWARE.
 ' 
-
+Imports DotNetNuke
 Imports DotNetNuke.Services.Exceptions
 
 ''' -----------------------------------------------------------------------------
