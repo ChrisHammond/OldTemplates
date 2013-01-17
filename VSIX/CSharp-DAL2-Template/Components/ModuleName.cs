6f122c54bb27acf7fe7826057b5d5a9e52d90fa7
christoc_cp <SND\christoc_cp>
2013-01-17 01:07:19 +0000

    Changes for DNN7 templates, two new project templates created for DAL2 support, not tested and complete yet

---
+++ VSIX/CSharp-DAL2-Template/Components/ModuleName.cs
@@ -0,0 +1,72 @@
+﻿/*
+' Copyright (c) $year$ $ownername$
+'  All rights reserved.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+' TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+' THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+' CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+' DEALINGS IN THE SOFTWARE.
+' 
+*/
+
+using System;
+using System.Web.Caching;
+using DotNetNuke.Common.Utilities;
+using DotNetNuke.ComponentModel.DataAnnotations;
+using DotNetNuke.Entities.Content;
+
+namespace $rootnamespace$$safeprojectname$.Components
+{
+    [TableName("$safeprojectname$_$safeprojectname$s")]
+    //setup the primary key for table
+    [PrimaryKey("$safeprojectname$Id", AutoIncrement = true)]
+    //configure caching using PetaPoco
+    [Cacheable("$safeprojectname$s", CacheItemPriority.Default, 20)]
+    //scope the objects to the ModuleId of a module on a page (or copy of a module on a page)
+    [Scope("ModuleId")]
+    class $safeprojectname$
+    {
+            ///<summary>
+        /// A string with the name of the $safeprojectname$Name
+        ///</summary>
+        public string $safeprojectname$Name { get; set; }
+
+        ///<summary>
+        /// A string with the description of the object
+        ///</summary>
+        public string $safeprojectname$Description { get; set; }
+
+        ///<summary>
+        /// An integeger with the user id of the assigned user for the object
+        ///</summary>
+        public int AssignedUserId { get; set; }
+
+        ///<summary>
+        /// The ModuleId of where the object was created and gets displayed
+        ///</summary>
+        public int ModuleId { get; set; }
+
+        ///<summary>
+        /// An integer for the user id of the user who created the object
+        ///</summary>
+        public int CreatedByUserId { get; set; }
+
+    ///<summary>
+        /// An integer for the user id of the user who last updated the object
+        ///</summary>
+        public int LastModifiedByUserId { get; set; }
+    
+    ///<summary>
+        /// The date the object was created
+        ///</summary>
+    public new DateTime CreatedOnDate { get; set; }
+
+    ///<summary>
+        /// The date the object was updated
+        ///</summary>
+        public new DateTime LastModifiedOnDate { get; set; }
+
+
+    }
+}
